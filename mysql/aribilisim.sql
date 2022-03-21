bb8c259deb72529ac58faf3d0109490fad863b42
Güney Askerova <guneyaskerova81@gmail.com>
2022-03-21 20:22:25 +0300

    sql

--- mysql/aribilisim.sql
+++ mysql/aribilisim.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1:3306
--- Üretim Zamanı: 21 Mar 2022, 16:51:52
--- Sunucu sürümü: 5.7.31
--- PHP Sürümü: 7.3.21
+-- Üretim Zamanı: 21 Mar 2022, 16:51:45
+-- Sunucu sürümü: 5.7.36
+-- PHP Sürümü: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `kategoriler`
+--
+
+DROP TABLE IF EXISTS `kategoriler`;
+CREATE TABLE IF NOT EXISTS `kategoriler` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(25) NOT NULL,
+  `katturu` varchar(15) NOT NULL,
+  `ustkat` varchar(30) NOT NULL,
+  `meta` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `kategoriler`
+--
+
+INSERT INTO `kategoriler` (`id`, `kategori`, `katturu`, `ustkat`, `meta`) VALUES
+(2, 'Web Tasarım', 'Ana Kategori', '-', 'Web Tasarım ve kodlama üzerine bilgiler'),
+(3, 'Grafik Tasarım', 'Ana Kategori', '-', 'Grafik Tasarım ve Prensipleri Hakkında Bilgiler'),
+(4, 'Dijital Pazarlama', 'Ana Kategori', '-', 'Sosyal Medya ve Google Süreçleri Hakkında Bilgiler'),
+(5, 'Html', 'Alt Kategori', 'Web Tasarım', 'Html, Bir Web Sitesinin Temel Kod Yapısıdır');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `ornek`
 --
 
@@ -34,17 +60,15 @@ CREATE TABLE IF NOT EXISTS `ornek` (
   `yas` int(3) NOT NULL,
   `il` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Tablo döküm verisi `ornek`
 --
 
 INSERT INTO `ornek` (`id`, `ad`, `yas`, `il`) VALUES
-(1, 'Hakan', 45, 'İzmir'),
-(2, 'Kaan', 40, 'İzmir'),
-(4, 'Damla', 33, 'Ankara'),
-(5, 'Mert', 24, 'Antalya');
+(6, 'Kaan', 40, 'İstanbul'),
+(8, 'Hayko', 40, 'İstanbul');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
