69bf39e31347d4ad90443d6bed8a0935e842280d
Güney Askerova <guneyaskerova81@gmail.com>
2022-03-21 19:44:22 +0300

    yeni dosya alındı

--- aribilgi/admin/notlar.php
+++
@@ -1,3 +0,0 @@
-Kaydetme işlemi için kullanılan - insert into tablo_adi(sutunadi1,sutunadi2,sutunadi3) values(?,?,?)
-Update - seçtiğimiz datanın güncellenmesini sağlar -> ör: update tablo_adi setsütunadı1 = ?, sütunadı2 = ?, sütunadı3 = ? örnek2: update ornek set ad='Hakan',yas=45,il='İzmir';
-select - veriyi (data) çekme, ekrana yazdırma (dizi ise) -> select*from tabloadı wehere (tek veri çekilecekse) örnek: select*from ornek;
